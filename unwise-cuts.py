6efeb47bed2173a7a30cfea819264ee12d9d692a
Dustin Lang <dstndstn@gmail.com>
2020-12-07 06:28:31 -0800

    more matches

--- unwise-cuts.py
+++ unwise-cuts.py
@@ -7,6 +7,375 @@ from astrometry.util.fits import *
 from astrometry.sdss.common import cas_flags, photo_flags1_map, photo_flags2_map
 from astrometry.libkd.spherematch import match_radec, tree_build_radec, tree_free, tree_search_radec, trees_match
 
+def li():
+    #(W1, W2, W3 and W4) for SDSS (galaxy) sources with 12 < Ra < 40 deg,  -2 < dec < 2 deg and redshift 0 < z < 0.2.
+    if False:
+        T = fits_table('sdss-dr13-tiles.fits')
+        photdir = 'sdss-dr13-phot'
+        margin = 0.8
+        T.cut((T.ra > 12-margin) * (T.ra < (40+margin)) * (T.dec > (-2-margin)) * (T.dec < (2+margin)))
+        print(len(T), 'tiles')
+        WW = []
+        for tile in T.coadd_id:
+            fn = os.path.join(photdir, 'phot-%s.fits' % tile)
+            if not os.path.exists(fn):
+                print('Does not exist:', fn)
+                continue
+            W = fits_table(fn)
+            print(len(W), 'from', fn)
+            if len(W) == 0:
+                continue
+            W.cut((W.ra > 12) * (W.ra < 40) * (W.dec > -2) * (W.dec < 2))
+            print('Cut to', len(W), 'in box')
+            WW.append(W)
+        W = merge_tables(WW)
+        W.writeto('li.fits')
+
+    S = fits_table('/global/cfs/cdirs/cosmo/data/sdss/dr13/sdss/spectro/redux/specObj-dr13.fits')
+    print(len(S), 'spectra')
+    S.about()
+    T = fits_table('li.fits.gz')
+    print(len(T), 'WISE')
+    T.about()
+    I,J,d = match_radec(S.plug_ra, S.plug_dec, T.ra, T.dec, 1., nearest=True)
+    print(len(I), 'match')
+    S.cut(I)
+    T.cut(J)
+    S.add_columns_from(T)
+    S.writeto('li-match.fits')
+    
+
+def kovlakas():
+    M = fits_table('SDSS12_objids.fits')
+    M.rename('radeg_sdss', 'ra')
+    M.rename('dedeg_sdss', 'dec')
+    # all_matches_near(None, 5., 'sdss-dr10d-tiles.fits',
+    #                  'sdss-dr10d-phot', 'kovlakas-unwise-dr10.fits', 'kovlakas', M=M, closest=True)
+    all_matches_near(None, 5., 'sdss-dr13-tiles.fits',
+                     'sdss-dr13-phot', 'kovlakas-unwise-dr13.fits', 'kovlakas', M=M, closest=True)
+
+def pizarro():
+    T = fits_table('cv-sources.fits')
+    T.rename('ra_deg_best', 'ra')
+    T.rename('dec_deg_best', 'dec')
+
+    # all_matches_near(None, 5.0, 'sdss-dr13-atlas.fits', 'sdss-dr13-phot',
+    #                  'cv-sdss-matches.fits', 'pizarro', M=T)
+                     
+    
+    cat_all_matches_near(T, 5., 'cv-catdr2-matches.fits', 'pizarro',
+                         catdir='/global/cfs/cdirs/cosmo/work/wise/unwise_catalog/dr2-cat/band-merged')
+
+def finn4():
+    all_matches_near('vf_north_v1_main.fits', 10.,
+                     'sdss-dr13-tiles.fits',
+                     'sdss-dr13-phot',
+                     'vf_north_v1_main-matched.fits', 'vf')
+
+def finn3():
+    if False:
+        all_matches_near('a100.SDSSObjID.191001.fits', 1.,
+                         'sdss-dr10d-tiles.fits', 'sdss-dr10d-phot',
+                         'a100.SDSSObjID.191001.match-dr10.fits', 'finn')
+
+    T = fits_table('a100.SDSSObjID.191001.fits')
+    M = fits_table('a100.SDSSObjID.191001.match.fits')
+    M.objid = np.array([int(o.strip()) for o in M.objid])
+    print('Targets:', len(T))
+    print('Matches:', len(M))
+
+    print('Unique objids in Targets:', len(set(T.objid)))
+    
+    I,J,d = match_radec(T.ra, T.dec, M.ra, M.dec, 1./3600.)
+    
+    K = np.flatnonzero(M.objid[J] == T.objid[I])
+    Tmatched = T[I[K]]
+    Mmatched = M[J[K]]
+    print(len(Tmatched), 'matched with objid')
+
+    print('Targets:')
+    Tmatched.about()
+    print('Matches:')
+    Mmatched.about()
+
+    Mmatched.writeto('a100.SDSSObjID.191001-matched-A.fits')
+
+    matched = np.zeros(len(T))
+    matched[I[K]] = True
+    TU = T[matched == False]
+    print(len(TU), 'un-matched')
+
+    I,J,d = match_radec(TU.ra, TU.dec, M.ra, M.dec, 2./3600, nearest=True)
+    print('Matched', len(I))
+
+    MU = M[J]
+    MU.writeto('a100.SDSSObjID.191001-matched-B.fits')
+    print(len(set(MU.objid).intersection(Mmatched.objid)), 'objids in common A-B')
+
+    matched = np.zeros(len(TU))
+    matched[I] = True
+    TUU = TU[matched == False]
+    print(len(TUU), 'un-matched')
+    TUU.writeto('a100.SDSSObjID.191001-unmatched.fits')
+
+    
+
+def finn2():
+    all_matches_near('vf_main.fits', 5.,
+                     'sdss-dr13-atlas.fits', 'sdss-dr13-phot',
+                     'vf_main-match.fits', 'vf')
+    
+def finn():
+    if False:
+        all_matches_near('a100.SDSSObjID.191001.fits', 1.,
+                         'sdss-dr13-atlas.fits', 'sdss-dr13-phot',
+                         'a100.SDSSObjID.191001.match.fits', 'finn')
+
+    T = fits_table('a100.SDSSObjID.191001.fits')
+    M = fits_table('a100.SDSSObjID.191001.match.fits')
+    print('Targets:', len(T))
+    print('Matches:', len(M))
+    
+    M.objid = np.array([int(o.strip()) for o in M.objid])
+    M.agc = T.agc[M.finn_index]
+    M.finn_objid = T.objid[M.finn_index]
+
+    M.writeto('a100.SDSSObjID.191001.match2.fits')
+    o1 = set(T.objid)
+    o2 = set(M.objid)
+    print('Unique targets:', len(o1))
+    print('Unique matches:', len(o2))
+    print('Union:', len(o1.union(o2)))
+    print('Intersection:', len(o1.intersection(o2)))
+
+    name = 'finn'
+    from collections import Counter
+    dups = Counter(zip(M.objid, M.get('%s_index' % name)))
+    print('Duplicate matches:', dups.most_common(5))
+    keep = np.ones(len(M), bool)
+    for (objid,ind),n in dups.most_common():
+        if n <= 1:
+            break
+        I = np.flatnonzero((M.objid == objid) * (M.get('%s_index' % name) == ind))
+        #print(len(I), 'for', objid, ind)
+        dists = np.hypot(M.x[I] - 1024.5, M.y[I] - 1024.5)
+        imin = np.argmin(dists)
+        ii = I[imin]
+        keep[I] = False
+        keep[ii] = True
+    M.cut(keep)
+    print('Cut to matches:', len(M))
+    o2 = set(M.objid)
+    print('Unique targets:', len(o1))
+    print('Unique matches:', len(o2))
+    print('Union:', len(o1.union(o2)))
+    print('Intersection:', len(o1.intersection(o2)))
+    K = np.flatnonzero(M.objid == M.finn_objid)
+    print(len(K), 'have matching objids')
+
+    #c = Counter(M.objid)
+    c = Counter(M.finn_objid)
+    hist_nmatches = Counter(c.values())
+    print('Number of matches per target:', hist_nmatches.most_common())
+    
+    M.writeto('a100.SDSSObjID.191001.match3.fits')
+
+    
+    
+
+def secrest():
+    # text2fits -H "id ra dec" -f jdd unWISE_request.ipac unWISE_request.fits
+    T = fits_table('unWISE_request.fits')
+
+    cat_all_matches_near(T, 5., 'unWISE_request_match.fits', 'secrest')
+    
+
+
+
+def cat_all_matches_near(M, radius, outfn, projname,
+                         catdir = '/global/project/projectdirs/cosmo/work/wise/unwise_catalog/dr1/band-merged',
+                         closest=False, **kwargs):
+    '''
+    radius in arcsec
+    '''
+    atlasfn = 'allsky-atlas.fits'
+    T = fits_table(atlasfn)
+    print(len(T), 'tiles')
+
+    #M = fits_table(catfn)
+    print(len(M), 'targets')
+
+    M.set('%s_index' % projname, np.arange(len(M)))
+
+    I,J,d = match_radec(M.ra, M.dec, T.ra, T.dec, 1.2)
+    keep = np.zeros(len(T), bool)
+    keep[J] = True
+    T.cut(keep)
+    print(len(T), 'tiles near targets')
+
+    WW = []
+    for t in T:
+        fn = os.path.join(catdir, '%s.cat.fits' % t.coadd_id)
+        if not os.path.exists(fn):
+            print('Does not exist:', fn)
+            continue
+        W = fits_table(fn)
+        print(len(W), 'from', fn)
+        if len(W) == 0:
+            continue
+
+        I,J,d = match_radec(M.ra, M.dec, W.ra, W.dec, radius/3600., **kwargs)
+        print(len(I), 'matched')
+        if len(I) == 0:
+            continue
+        W.cut(J)
+        MI = M[I]
+        MI.rename('ra',  'ra_%s'  % projname)
+        MI.rename('dec', 'dec_%s' % projname)
+        W.add_columns_from(MI)
+        WW.append(W)
+
+    W = merge_tables(WW)
+    print(len(W), 'total matches')
+    W = W[np.argsort(W.get('%s_index' % projname))]
+
+    print(len(np.unique(W.unwise_objid)), 'unique objids')
+
+    if closest:
+        from collections import Counter
+        dups = Counter(zip(W.unwise_objid, W.get('%s_index' % projname)))
+        print('Duplicate matches:', dups.most_common(5))
+        keep = np.ones(len(W), bool)
+        for (objid,ind),n in dups.most_common():
+            if n <= 1:
+                break
+            I = np.flatnonzero((W.unwise_objid == unwise_objid) * (W.get('%s_index' % projname) == ind))
+            print(len(I), 'for', objid, ind)
+            dists = np.hypot(W.x[I] - 1024.5, W.y[I] - 1024.5)
+            imin = np.argmin(dists)
+            ii = I[imin]
+            keep[I] = False
+            keep[ii] = True
+        W.cut(keep)
+
+    W.writeto(outfn)
+
+    
+
+def hamadouche():
+    if False:
+        T = fits_table('hamadouche.fits')
+        T.rename('ras', 'ra')
+        T.rename('decs', 'dec')
+        T.rename('objid', 'hamadouche_objid')
+        T.writeto('hamadouche2.fits')
+        all_matches_near('hamadouche2.fits', 1.0, 'sdss-dr13-atlas.fits',
+                         'sdss-dr13-phot', 'table1forunWISE-matched.fits', 'hamadouche')#, closest=True)
+    M = fits_table('table1forunWISE-matched.fits')
+    M.objid = np.array([int(o) for o in M.objid])
+    print(len(M), 'matches,', len(np.unique(M.objid)), 'objids')
+    M.cut(M.hamadouche_objid == M.objid)
+    print(len(M), 'with matching objids')
+    o,I = np.unique(M.hamadouche_objid, return_index=True)
+    M.cut(I)
+    print(len(M), 'unique matching objids')
+    M.writeto('table1forunWISE-matched2.fits')
+
+
+
+
+def frasermckelvie():
+    all_matches_near('MWAG_coords.fits', 1.0, 'sdss-dr13-atlas.fits',
+                     'sdss-dr13-phot', 'fm.fits', 'frasermckelvie', closest=True)
+
+def frasermckelvie2():
+    all_matches_near('mwa.fits', 1.0, 'sdss-dr13-atlas.fits',
+                     'sdss-dr13-phot', 'fm2.fits', 'frasermckelvie', closest=True)
+
+def mccleary2():
+    # A random subsample of 200k galaxies over the sky with SDSS & WISE mags/fluxes
+    np.random.seed(42)
+
+    # # 8k tiles
+    # A = fits_table('sdss-dr13-atlas.fits')
+    # # Choose tiles randomly
+    # I = np.random.permutation(len(A))
+    # A.cut(I)
+    # for tile in A.coadd_id:
+
+    W = fits_table('window_flist-cut.fits', columns=['run','camcol','field'])
+    print('Read', len(W), 'fields')
+    # Choose SDSS fields randomly
+    I = np.random.permutation(len(W))
+    W.cut(I)
+
+    os.environ['BOSS_PHOTOOBJ'] = '/global/project/projectdirs/cosmo/data/sdss/dr13/eboss/photoObj/'
+
+    ntotal = 0
+    TT = []
+    for i,(run,camcol,field) in enumerate(zip(W.run, W.camcol, W.field)):
+        print()
+        print(i+1, 'of', len(W), ': Run,camcol,field', run,camcol,field)
+        fn = os.path.join(os.environ['BOSS_PHOTOOBJ'], '301', '%i'%run,
+                          '%i'%camcol, 'photoObj-%06i-%i-%04i.fits' % (run,camcol,field))
+    
+        T = fits_table(fn, columns=['objc_type', 'resolve_status',])
+        if T is None:
+            continue
+        T.index = np.arange(len(T))
+        T.cut((T.resolve_status & 256) > 0)
+        T.cut(T.objc_type == 3)
+        print('Reading', len(T), 'from', fn)
+        if len(T) == 0:
+            continue
+    
+        # Re-read SDSS photoObjs and grab more columns.
+        inds = T.index
+        T = fits_table(fn, rows=inds, columns=['run','camcol','field',
+                                               'ra','dec','raerr','decerr',
+                                               'cmodelmag', 'cmodelmagerr',
+                                               'modelmag', 'modelmagerr',
+                                               'psfmag','psfmagerr',
+                                               'flags', 'flags2', 'objid',
+                                               'modelflux', 'modelflux_ivar',
+                                               'cmodelflux', 'cmodelflux_ivar'])
+    
+        unwdir = '/global/project/projectdirs/cosmo/data/unwise/allwise/unwise-phot/sdss-dr13-pobj'
+        fn = os.path.join(unwdir, '%i'%run, '%i'%camcol, 'photoWiseForced-%06i-%i-%04i.fits' % (run, camcol, field))
+        print('Reading from', fn)
+        U = fits_table(fn, rows=inds, columns=['treated_as_pointsource', 'pointsource',
+                                               'w1_mag', 'w1_mag_err',
+                                               'w2_mag', 'w2_mag_err',
+                                               'w3_mag', 'w3_mag_err',
+                                               'w4_mag', 'w4_mag_err',
+                                               'w1_nanomaggies', 'w1_nanomaggies_ivar',
+                                               'w2_nanomaggies', 'w2_nanomaggies_ivar',
+                                               'w3_nanomaggies', 'w3_nanomaggies_ivar',
+                                               'w4_nanomaggies', 'w4_nanomaggies_ivar',
+                                               'has_wise_phot', 'objid'])
+        print('has_wise_phot:', np.unique(U.has_wise_phot))
+        # *almost* all the time, has_wise_phot=True; exception is
+        # sdss-dr13-pobj/4135/4/photoWiseForced-004135-4-0169.fits
+        # but I haven't investigated.
+        T.cut(U.has_wise_phot)
+        U.cut(U.has_wise_phot)
+    
+        assert(np.all(T.objid == U.objid))
+    
+        U.delete_column('objid')
+        T.add_columns_from(U)
+    
+        TT.append(T)
+        ntotal += len(T)
+        print('Total:', ntotal)
+        if ntotal > 200000:
+            break
+    
+    T = merge_tables(TT)
+    T[:200000].writeto('mccleary2.fits')
+
+
+
 def seuss():
     #all_matches_near('psbs.fits', 1.0, 'sdss-dr13-atlas.fits',
     #                 'sdss-dr13-phot', 'psbs-unwise.fits', 'seuss')
@@ -29,8 +398,6 @@ def seuss():
 
     T.cut(keep)
     T.writeto('psbs-unwise-2.fits')
-
-
 
 def lodieu():
     #for fn in ['mytable1.fits', 'mytable2.fits', 'mytable3.fits']:
@@ -561,16 +928,21 @@ def nunez():
 
 
 
-def all_matches_near(catfn, radius, atlasfn, photdir, outfn, projname, **kwargs):
+def all_matches_near(catfn, radius, atlasfn, photdir, outfn, projname,
+                     closest=False, M=None, **kwargs):
     '''
     radius in arcsec
     '''
     T = fits_table(atlasfn)
     print(len(T), 'tiles')
 
-    M = fits_table(catfn)
+    if M is None:
+        M = fits_table(catfn)
     print(len(M), 'targets')
 
+    if closest:
+        kwargs.update(nearest=True)
+    
     M.set('%s_index' % projname, np.arange(len(M)))
 
     I,J,d = match_radec(M.ra, M.dec, T.ra, T.dec, 1.2)
@@ -609,6 +981,23 @@ def all_matches_near(catfn, radius, atlasfn, photdir, outfn, projname, **kwargs)
     W = W[np.argsort(W.get('%s_index' % projname))]
 
     print(len(np.unique(W.objid)), 'unique objids')
+
+    if closest:
+        from collections import Counter
+        dups = Counter(zip(W.objid, W.get('%s_index' % projname)))
+        print('Duplicate matches:', dups.most_common(5))
+        keep = np.ones(len(W), bool)
+        for (objid,ind),n in dups.most_common():
+            if n <= 1:
+                break
+            I = np.flatnonzero((W.objid == objid) * (W.get('%s_index' % projname) == ind))
+            print(len(I), 'for', objid, ind)
+            dists = np.hypot(W.x[I] - 1024.5, W.y[I] - 1024.5)
+            imin = np.argmin(dists)
+            ii = I[imin]
+            keep[I] = False
+            keep[ii] = True
+        W.cut(keep)
 
     W.writeto(outfn)
 
@@ -686,8 +1075,18 @@ def razink():
 
 if __name__ == '__main__':
     import sys
-
-    seuss()
+    li()
+    #finn4()
+    #finn3()
+    #finn2()
+    #kovlakas()
+    #pizarro()
+    #finn()
+    #secrest()
+    #hamadouche()
+    #frasermckelvie2()
+    #mccleary2()
+    #seuss()
     #cerulo()
     #razink()
     #lai()
